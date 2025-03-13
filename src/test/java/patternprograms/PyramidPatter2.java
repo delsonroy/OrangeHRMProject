610dcbf8ea0a6e393d45c3bd1408d24041b90c37
delsonroy <delson.geo@gmail.com>
2025-03-13 15:03:35 -0400

    This is the latest commit

---
+++ src/test/java/patternprograms/PyramidPatter2.java
@@ -0,0 +1,29 @@
+package patternprograms;
+
+public class PyramidPatter2 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=10; i++)
+		{
+			for(int space =1; space<=10-i; space ++)
+			{
+				System.out.print(" ");
+			}
+			
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(j);
+			}
+			for(int j=i-1; j>=1; j--)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+			
+		}
+		
+
+	}
+
+}
