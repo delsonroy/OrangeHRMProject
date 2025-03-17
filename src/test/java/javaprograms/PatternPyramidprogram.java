9b2a575ad4b4737e9b084453ae47d3e1f20158e1
delsonroy <delson.geo@gmail.com>
2025-03-17 10:47:03 -0400

    changed the pattern program

--- src/test/java/javaprograms/PatternPyramidprogram.java
+++ src/test/java/javaprograms/PatternPyramidprogram.java
@@ -17,7 +17,7 @@ public class PatternPyramidprogram {
 			for (int j = 1; j <= i; j++) {
 				System.out.print(j);
 			}
-			for (int j = 1 - 1; j >= 1; j--) {
+			for (int j = i - 1; j >= 1; j--) {
 				System.out.print(j);
 			}
 			System.out.println();
