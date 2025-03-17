5b7d63f1e2e944b126d0143ba22badc80f681fae
delsonroy <delson.geo@gmail.com>
2025-03-16 20:45:46 -0400

    new cmmit

---
+++ src/test/java/javaprograms/PatternProgram.java
@@ -0,0 +1,20 @@
+package javaprograms;
+
+public class PatternProgram {
+
+	public static void main(String[] args) {
+
+		
+		for(int i=2; i<=10; i++)
+		{
+			for(int j=i-1; j>=1; j--)
+			{
+				
+				System.out.print(j);
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
---
+++ src/test/java/javaprograms/PatternPyramidprogram.java
@@ -0,0 +1,28 @@
+package javaprograms;
+
+public class PatternPyramidprogram {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++)
+
+		{
+			for (int space = 1; space <= 10 - i; space++)
+				
+			{
+
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j);
+			}
+			for (int j = 1 - 1; j >= 1; j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+
+	}
+}
